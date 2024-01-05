3147e271c59c74ca704bb987d87b6642f0800959
Missael Hernandez Rosado <missael.hernandez@contrastsecurity.com>
2024-01-04 22:58:38 -0600

    TASK-0 Python script with tests

--- cloudengineer-project/flask/vuln.py
+++ cloudengineer-project/flask/vuln.py
@@ -5,7 +5,6 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # AWS S3 configuration
-AWS_REGION = 'us-east-1'
 S3_BUCKET_NAME = 'missael-hire-project'
 S3_FILE_KEY = 'example.json'
 
@@ -13,13 +12,15 @@ S3_FILE_KEY = 'example.json'
 def process_json_data():
     try:
         # Download the JSON file from S3
-        s3_client = boto3.client('s3', region_name=AWS_REGION)
-        response = s3_client.get_object(Bucket=S3_BUCKET_NAME, Key=S3_FILE_KEY)
-        json_data = json.loads(response['Body'].read().decode('utf-8'))
+        s3 = boto3.resource('s3')
+        content_object = s3.Object(S3_BUCKET_NAME, S3_FILE_KEY)
+        file_content = content_object.get()['Body'].read().decode('utf-8')
+        json_data = json.loads(file_content)
+        vulnerabilities = json_data['vulnerabilities']
 
         # Process JSON data and calculate vulnerability counts per vendor_id
         result_data = {}
-        for entry in json_data:
+        for entry in vulnerabilities:
             vendor_id = entry.get('vendor_id')
             severity = entry.get('severity', 'UNKNOWN')
 
@@ -36,7 +37,7 @@ def process_json_data():
         return None
 
 # Flask route to trigger the processing and return results
-@app.route('/process_and_return_results', methods=['GET'])
+@app.route('/results', methods=['GET'])
 def process_and_return_results():
     result_data = process_json_data()
     if result_data is not None:
