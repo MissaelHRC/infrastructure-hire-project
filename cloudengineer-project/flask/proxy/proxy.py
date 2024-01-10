2f8dc215aee0c8717ac14b906a6dc318e49a8ed9
Missael Hernandez Rosado <missael.hernandez@contrastsecurity.com>
2024-01-10 00:36:35 -0600

    TASK-7 Fix redirect proxy

--- cloudengineer-project/flask/proxy/proxy.py
+++ cloudengineer-project/flask/proxy/proxy.py
@@ -9,7 +9,7 @@ def health_check():
 
 @app.route('/vulns')
 def vulnerability_proxy():
-    return redirect("http://localhost:8001/results")
+    return redirect("http://127.0.0.1:8001/results")
 
 @app.route('/evil')
 def evil_proxy():
