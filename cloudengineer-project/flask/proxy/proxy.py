6a4145dfec2143d5069c83ed52c4292c2f4bebf4
Missael Hernandez Rosado <missael.hernandez@contrastsecurity.com>
2024-01-10 00:56:05 -0600

    TASK-9 update host to use pod name

--- cloudengineer-project/flask/proxy/proxy.py
+++ cloudengineer-project/flask/proxy/proxy.py
@@ -9,7 +9,7 @@ def health_check():
 
 @app.route('/vulns')
 def vulnerability_proxy():
-    get("http://127.0.0.1:8001/results")
+    get("http://app:8001/results")
 
 @app.route('/evil')
 def evil_proxy():
