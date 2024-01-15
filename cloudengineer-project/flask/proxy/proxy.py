ae775016482457e80bc811c23f64effaed346e09
Missael Hernandez Rosado <missael.hernandez@contrastsecurity.com>
2024-01-15 12:21:03 -0600

    TASK-14 fix nginx resource and improve proxy

--- cloudengineer-project/flask/proxy/proxy.py
+++ cloudengineer-project/flask/proxy/proxy.py
@@ -14,14 +14,17 @@ def vulnerability_proxy():
     port = os.environ.get('APP_PORT', '8001')
     url = f'http://{host}:{port}/results'
     response = requests.get(url)
-    print(response)
     data = response.json()
     return data
 
 @app.route('/evil')
 def evil_proxy():
-    response = requests.get("http://nginx-project:8002")
-    return response.text
+    host = os.environ.get('PROXY_HOST', 'localhost')
+    port = os.environ.get('PROXY_PORT', '8002')
+    url = f'http://{host}:{port}/evil'
+    #response = requests.get(url)
+    #return response.text
+    get(url)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
